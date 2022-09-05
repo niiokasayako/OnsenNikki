498ca4d5f2c3d1bd451296af27dc2c16adb129e2
EC2 Default User <ec2-user@ip-172-31-45-224.ap-northeast-1.compute.internal>
2022-09-05 06:42:09 +0000

    [Add]devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+
+ def after_sign_in_path_for(resource)
+    public_root_path
+ end
+
+ def after_sign_out_path_for(resource)
+    public_root_path
+ end
+
 end
