35664af1f914ff5028b5b302fa4ca0696ddd45c6
EC2 Default User <ec2-user@ip-172-31-45-224.ap-northeast-1.compute.internal>
2022-09-05 08:34:59 +0000

    [Add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'reviews/show'
-    get 'reviews/edit'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/edit'
-  end
-  namespace :admin do
-    get 'informations/index'
-    get 'informations/edit'
-  end
-  namespace :admin do
-    get 'onsens/new'
-    get 'onsens/show'
-    get 'onsens/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'reviews/new'
-    get 'reviews/edit'
-    get 'reviews/show'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/unsubscribe'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'onsens/index'
-    get 'onsens/show'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
+
  # 顧客用
 # URL /users/sign_in ...
 devise_for :users, controllers: {
@@ -50,6 +12,27 @@ devise_for :users, controllers: {
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :public do
+    root to: "homes#top"
+    get 'users/show' => "users#show", as: "users/my_page"
+    get 'users/edit' => "users#edit", as: "users/information/edit"
+    patch 'users/update' => "users#update", as: "users/information"
+    get 'users/unsubscribe'
+    patch 'users/withdraw'
+    resources :reviews, only: [:new, :edit, :show, :create, :update, :destroy]
+    resources :onsens, only: [:index, :show]
+    resources :post_comments, only: [:create, :destroy]
+    resources :samps, only: [:create, :destroy]
+  end
+
+  namespace :admin do
+    get '/' => "homes#top", as: "/"
+    resources :onsens, only: [:new, :edit, :show, :create, :update]
+    resources :informations, only: [:index, :edit, :create, :update]
+    resources :users, only: [:index, :edit, :update]
+    resources :reviews, only: [:edit, :show, :update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
