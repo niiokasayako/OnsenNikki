0d111551ab12bdde78269ac1cbbb59f93d855292
niiokasayako <107908957+niiokasayako@users.noreply.github.com>
2022-09-28 09:56:44 +0000

    [Fix]routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
 
  def after_sign_in_path_for(resource)
-    public_path
+    public_root_path
  end
 
  def after_sign_out_path_for(resource)
-    public_path
+    public_rootpath
  end
 
 protected
