e2729674b1a3338a4ec2928ca407c36a2c99c006
niiokasayako <107908957+niiokasayako@users.noreply.github.com>
2022-09-29 07:12:16 +0000

    [Fix]homesController map

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,8 +4,8 @@ class HomesController < ApplicationController
 
   def top
     @onsen = Onsen.first
-    # マーカーを半径10km圏内で取得表示する
-    @onsens = Onsen.where(latitude: (@onsen.latitude - 0.05)..(@onsen.latitude + 0.05), longitude: (@onsen.longitude - 0.05)..(@onsen.longitude + 0.05))
+    # マーカーを半径200km圏内で取得表示する
+    @onsens = Onsen.where(latitude: (@onsen.latitude - 2)..(@onsen.latitude + 2), longitude: (@onsen.longitude - 2)..(@onsen.longitude + 2))
     gon.onsens = @onsens
   end
 
